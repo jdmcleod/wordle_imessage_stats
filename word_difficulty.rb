daacc651c3781e6aa07961c84213f2af4fb3b9f7
Josh McLeod <josh@mcleodfamily.org>
2025-04-24 13:01:56 -0400

    Todays word difficulty

--- word_difficulty.rb
+++ word_difficulty.rb
@@ -4,7 +4,6 @@ require 'csv'
 require 'date'
 require_relative 'wordle_chat_parser'
 
-
 begin
   first_line = CSV.read('history.csv').first
   unless first_line && Date.parse(first_line[0]) == Date.today
@@ -19,14 +18,21 @@ grouped = worldes.group_by(&:answer)
 
 total = grouped.count
 
-by_difficulty = grouped.map do |answer, wordles|
+words = grouped.map do |answer, wordles|
   average_score = (wordles.sum(&:score_for_average) / wordles.count.to_f).round(2)
   [answer, average_score, wordles.first.date.strftime('%b %d %Y')]
 end
 
-sorted = by_difficulty.sort_by { _1[1] }
+todays_word = words.last.first
+
+sorted = words.sort_by { _1[1] }
 formatted = sorted.map.with_index { "#{total - _2}. #{_1.join(', ')}" }
 
 puts "#{formatted.length} wordles"
 
+todays_index = sorted.index { _1.first == todays_word }
+todays_difficulty_percentile = ((todays_index.to_f / total.to_f) * 100.0).round
+
 puts formatted
+
+puts "\n⏰ Today's word is #{todays_word} and was in the #{todays_difficulty_percentile}% of difficulty"
